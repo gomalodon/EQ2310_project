f51c4ae381d6f5ad7dd19cf6a1b9d32bad014bbd
gomalodon <gielooghe@hotmail.be>
2023-01-27 21:00:18 +0100

    detect done

--- detect.m
+++ detect.m
@@ -20,5 +20,17 @@ function bhat = detect(r)
 %
 % Output:
 %   bhat  = bits {0,1} corresponding to the QPSK symbols
-
-
+n = size(r,2);
+bhat = zeros(1,2*n);
+for i=1:n
+    if real(r(i)) > 0
+        bhat(2*i-1) = 0;
+    else
+        bhat(2*i-1) = 1;
+    end
+    if imag(r(i)) > 0
+        bhat(2*i) = 0;
+    else
+        bhat(2*i) = 1;
+    end
+end
